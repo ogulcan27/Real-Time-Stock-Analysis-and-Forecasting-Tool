3e686fda3db4c85e3a991a9741026dd54bcd311a
Oğulcan Özer <89734958+ogulcan27@users.noreply.github.com>
2025-02-04 15:59:57 +0300

    Delete get_stock_prices_history.py

--- get_stock_prices_history.py
+++
@@ -1,8 +0,0 @@
-def get_stock_prices_history(ticker, period):
-    stock = yf.Ticker(ticker)
-    history = stock.history(period=period)
-    if history.empty:
-        return None 
-    start_price = history["Close"].iloc[0]
-    end_price = history["Close"].iloc[-1]
-    return {"start_price": start_price, "end_price": end_price}
